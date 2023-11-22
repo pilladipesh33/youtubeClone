f42ef3d78c4c6e65b9b2f9a912d767260a904b22
Pilla Dipesh <pilladipesh.pd@gmail.com>
2023-11-22 09:29:28 +0530

    inital changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,28 @@
-import { Icon } from "@mui/material";
 import Logo from "../../assets/svg/logo";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchBar from "../searchBar/SearchBar";
 import VideoCam from "../../assets/svg/videoCam";
 import Notification from "../../assets/svg/notification";
+import { useState } from "react";
+import MiniDrawer from "./Drawer";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  function handleDrawer(){
+    if (open == true){
+      setOpen(false);
+    } 
+    else {
+      setOpen(true);
+    }
+  }
+
   return (
-    <div className="flex bg-stone-500 h-14 bg-white w-full p-3 items-center justify-between">
+    <div className="flex h-14 bg-white w-full p-3 items-center justify-between">
       <div className="px-6 flex">
-        <MenuIcon />
+        <MenuIcon onClick = {() => handleDrawer()}/>
         <div className="ps-8">
-        <Logo />
+          <Logo />
         </div>
       </div>
       <div className="px-80">
